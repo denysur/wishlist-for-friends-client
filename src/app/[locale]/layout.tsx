036aa92d9a0325fafc4466dffcec8784d9bd1118
Denys Urla <urladenys.dev.com>
2025-05-06 11:51:31 +0300

    WFF-29: Use inter font

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import { ThemeProvider } from "@mui/material";
 import "@/globals.css";
 import theme from "@/theme";
@@ -7,16 +7,7 @@ import { Locale } from "@/types";
 import { dir } from "i18next";
 import { Metadata } from "next";
 
-const archivoBlack = localFont({
-  src: "../../fonts/ArchivoBlack-Regular.ttf",
-  variable: "--font-archivo-black",
-  weight: "400",
-});
-const notoSans = localFont({
-  src: "../../fonts/NotoSans-VariableFont_wdth,wght.ttf",
-  variable: "--font-noto-sans",
-  weight: "400",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Wishlist For Friends",
@@ -33,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang={locale} dir={dir(locale)}>
-      <body className={`${archivoBlack.variable} ${notoSans.variable}`}>
+      <body className={inter.className}>
         <ThemeProvider theme={theme}>
           <AppRouterCacheProvider options={{ enableCssLayer: true }}>
             {children}
